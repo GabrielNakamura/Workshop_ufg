a67c3c1279b3b9acdf57eaf9b39d13bdef9dd5d3
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2023-04-11 18:17:34 -0300

    adding data for examples

---
+++ data_example/My_messy_script.R
@@ -0,0 +1,26 @@
+remotes::install_github("allisonhorst/palmerpenguins")
+
+
+library(palmerpenguins)
+library(dplyr)
+library(ggplot2)
+theme_set(theme_minimal())
+write.csv2(penguins, here::here("data_example", "raw_data_penguins.csv"))
+
+data_processed <- 
+penguins %>%
+  dplyr::select(where(is.factor)) %>% 
+  glimpse()
+write.csv2(x = data_processed, file = "/Users/gabrielnakamura/OneDrive/Aulas/Workshop_UofT/data_example/data_processed1.csv")
+
+
+plot_1 <- 
+ggplot(penguins, aes(x = island, fill = species)) +
+  geom_bar(alpha = 0.8) +
+  scale_fill_manual(values = c("darkorange","purple","cyan4"), 
+                    guide = FALSE) +
+  theme_minimal() +
+  facet_wrap(~species, ncol = 1) +
+  coord_flip()
+ggplot2::ggsave(filename = "/Users/gabrielnakamura/OneDrive/Aulas/Workshop_UofT/data_example/plot_1", plot = plot_1, device = "png", dpi = "screen")
+
---
+++ data_example/my_messy_script2.R
@@ -0,0 +1,23 @@
+# Count penguins for each species / sex
+
+
+data_processed2 <- 
+  penguins %>% 
+  count(species, sex, .drop = FALSE)
+
+
+ggplot(penguins, aes(x = sex, fill = species)) +
+  geom_bar(alpha = 0.8) +
+  scale_fill_manual(values = c("darkorange","purple","cyan4"), 
+                    guide = FALSE) +
+  theme_minimal() +
+  facet_wrap(~species, ncol = 1) +
+  coord_flip()
+
+
+# Scatterplot example 1: penguin flipper length versus body mass
+ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(aes(color = species, 
+                 shape = species),
+             size = 2) +
+  scale_color_manual(values = c("darkorange","darkorchid","cyan4")) 
